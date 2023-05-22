f22d9408c3eabcff45624fbd3a70d5a5309719c6
Sandrina Pereira <a.sandrina.p@gmail.com>
2023-05-22 19:03:58 +0100

    ex7: start

--- exercises/7_advanced_keyboard.exercise.jsx
+++ exercises/7_advanced_keyboard.exercise.jsx
@@ -1,10 +1,70 @@
 import React from "react";
+import { css } from "styled-components";
 
-import { buttonCSS, buttonOutlineCSS } from "../src/components/Button";
-import { Case, StackXCenter, rotateCSS } from "../src/components/Layout";
+import { buttonOutlineCSS } from "../src/components/Button";
+import { Case, Stack } from "../src/components/Layout";
+import { people } from "../src/utils/people.js";
 
-function CaseTabs() {
-  return <Case title="Shortcuts">coming</Case>;
+function CaseRoving() {
+  return (
+    <Case title="Roving technique">
+      <h3>Create "Checkout" Team</h3>
+
+      <p>
+        Here's a <a href="#">link</a> just for example.
+      </p>
+
+      <table css={stylesTable}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job Role</th>
+            <th>City</th>
+            <th>
+              <span className="sr-only">Select</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map((p) => {
+            return (
+              <tr key={p.userId}>
+                <td>
+                  <a
+                    //
+                    href={`#${p.userId}`}
+                  >
+                    {p.name}
+                  </a>
+                </td>
+                <td>{p.jobRole}</td>
+                <td>
+                  <a
+                    //
+                    href={p.cityUrl}
+                  >
+                    {p.city}
+                  </a>
+                </td>
+                <td>
+                  <input
+                    type="checkbox"
+                    name="people"
+                    value={p.userId}
+                    aria-label="Promote"
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <Stack justifyContent="end">
+        <button css={buttonOutlineCSS}>Next step</button>
+      </Stack>
+    </Case>
+  );
 }
 
 // ========================
@@ -23,22 +83,69 @@ function CaseTabs() {
 // *
 // *
 
+export const stylesTable = css`
+  border: 1px solid;
+  border-collapse: collapse;
+  width: 100%;
+  margin: 16px 0;
+
+  th {
+    padding: 4px 8px;
+    text-align: left;
+    background: var(--theme-text_0);
+    color: var(--theme-text_invert);
+  }
+
+  td {
+    padding: 4px 8px;
+    border-bottom: 1px solid;
+  }
+
+  tr:nth-child(even) {
+    background: #eee;
+  }
+
+  tr:focus-within {
+    background: #cdcdff;
+    outline: 2px solid #3f51b5;
+  }
+
+  th:last-child,
+  td:last-child {
+    width: 50px;
+  }
+`;
+
 export const cases = [
   {
-    id: "tabs",
-    Exercise: CaseTabs,
-    briefing: ` `,
+    id: "roving",
+    Exercise: CaseRoving,
+    briefing: `
+Nagivating this table demands a lot of Tab, Tab Tab. 
+Can we make it better? Yes, with the "Roving Tabindex" technique.   
+
+"Roving tabindex" is a strategy to maximize the UX efficiency as much
+as possible when navigating a complex app with a keyboard.
+
+With "Roving" you treat complex components as a Single Tab stop, so
+the user can quickly bypass it by pressing Tab again. To navigate 
+within the complex component, they'll use other keys, for instance, the Arrow keys.
+
+This technique is common in Menus, Tabs, and even long Nav bars. 
+        `,
     resources: [
       {
-        name: "React Hook: usePrefersReducedMotion()",
-        url: "https://www.joshwcomeau.com/snippets/react-hooks/use-prefers-reduced-motion/",
+        name: "Video: Roving tabindex",
+        url: "https://www.youtube.com/watch?v=uCIC2LNt0bk",
       },
       {
-        name: "no-motion-first approach",
-        url: "https://www.tatianamac.com/posts/prefers-reduced-motion/",
-        extra: true,
+        name: "Demo: Toolbar by WAI",
+        url: "https://www.w3.org/WAI/ARIA/apg/patterns/toolbar/examples/toolbar/",
+      },
+      {
+        name: "Roving in React",
+        url: "https://www.joshuawootonn.com/react-roving-tabindex",
       },
     ],
-    briefing_bonus: ``,
   },
 ];
--- exercises/7_advanced_keyboard.solution.jsx
+++ exercises/7_advanced_keyboard.solution.jsx
@@ -1,10 +1,151 @@
 import React from "react";
 
-import { buttonCSS, buttonOutlineCSS } from "../src/components/Button";
-import { Case, StackXCenter, rotateCSS } from "../src/components/Layout";
+import { buttonOutlineCSS } from "../src/components/Button";
+import { Case, Stack } from "../src/components/Layout";
+import { people } from "../src/utils/people.js";
+import { stylesTable } from "./7_advanced_keyboard.exercise";
 
-function CaseTabs() {
-  return <Case title="Shortcuts">coming</Case>;
+const RovingContext = React.createContext({});
+
+const arrowCode = {
+  37: "LEFT",
+  38: "UP",
+  39: "RIGHT",
+  40: "DOWN",
+};
+
+function RovingContainer({ children, maxIndex }) {
+  const [activeIx, setActiveIx] = React.useState(0);
+  const registeredFocusOnRoverEls = React.useRef({});
+
+  function registerFocusOnRoverEl(ix, el) {
+    registeredFocusOnRoverEls.current = {
+      ...registeredFocusOnRoverEls.current,
+      [ix]: el,
+    };
+  }
+
+  function handleKeyDown(e) {
+    const arrowType = arrowCode[e.keyCode]; // LEFT || UP || RIGHT || DOWN
+
+    if (!arrowType) {
+      // Do nothing if they didn't click an arrow key
+      return;
+    }
+
+    e.preventDefault(); // Prevent page scroll
+
+    let newIx;
+
+    if (arrowType === "UP" || arrowType === "LEFT") {
+      if (activeIx === 0) {
+        // top of the list
+        return; // Or you could set the last index (loop).
+      }
+      newIx = activeIx - 1;
+    }
+
+    if (arrowType === "DOWN" || arrowType === "RIGHT") {
+      if (activeIx === maxIndex) {
+        // bottom of the list.
+        return; // Or you could set the first index (loop).
+      }
+      newIx = activeIx + 1;
+    }
+
+    // 💡 Update the new active index!
+    setActiveIx(newIx);
+
+    // 💡 Move the focus() to the new active element
+    registeredFocusOnRoverEls.current[newIx]?.focus();
+  }
+
+  return (
+    <RovingContext.Provider value={{ activeIx, registerFocusOnRoverEl }}>
+      {React.cloneElement(children, {
+        onKeyDown: handleKeyDown,
+      })}
+    </RovingContext.Provider>
+  );
+}
+
+function RovingItem({ children, index, focusOnRover }) {
+  const elRef = React.useRef();
+  const { activeIx, registerFocusOnRoverEl } = React.useContext(RovingContext);
+  const isFocusable = activeIx === index;
+
+  React.useEffect(() => {
+    if (focusOnRover) {
+      // This will be the element that will receive .focus()
+      // when the rover (tabindex="0") happens to it.
+      registerFocusOnRoverEl(index, elRef.current);
+    }
+  });
+
+  return React.cloneElement(children, {
+    tabIndex: isFocusable ? 0 : -1,
+    ref: elRef,
+  });
+}
+
+function CaseRoving() {
+  return (
+    <Case title="Roving technique">
+      <p>
+        Here's a <a href="#a">link</a> just for example.
+      </p>
+
+      <h3>Create "Checkout" Team</h3>
+
+      <RovingContainer maxindex={people.length - 1}>
+        <table css={stylesTable}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Job Role</th>
+              <th>City</th>
+              <th>
+                <span className="sr-only">Select</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {people.map((p, ix) => {
+              return (
+                <tr key={p.userId}>
+                  <td>
+                    <RovingItem index={ix} focusOnRover>
+                      <a href={`#${p.userId}`}>{p.name}</a>
+                    </RovingItem>
+                  </td>
+                  <td>{p.jobRole}</td>
+                  <td>
+                    <RovingItem index={ix}>
+                      <a href={p.cityUrl}>{p.city}</a>
+                    </RovingItem>
+                  </td>
+                  <td>
+                    <RovingItem index={ix}>
+                      <input
+                        type="checkbox"
+                        name="people"
+                        value={p.userId}
+                        aria-label="Promote"
+                      />
+                    </RovingItem>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </RovingContainer>
+
+      <Stack justifyContent="end">
+        <button css={buttonOutlineCSS}>Next step</button>
+      </Stack>
+    </Case>
+  );
 }
 
 // ========================
@@ -25,7 +166,36 @@ function CaseTabs() {
 
 export const solutions = [
   {
-    Solution: CaseTabs,
+    Solution: CaseSkip,
+    explanation: `
+The solution requires two elements: A trigger and a target.
+
+- Trigger: a link at the top of the DOM pointing to the target by id
+
+      <a href="#content" class="skipLink">
+        Skip to main content
+      </a>
+
+- Target: The content element with the id matching the same name. Older browsers need \`tabindex="-1"\` too.
+
+      <main id="content" tabindex="-1">...</main>
+
+Finally we need to visually hide the skip links by default and only show it when focused.
+
+    .skipLink:not(:focus) {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      /* rest of .sr-only styles... */
+    }
+
+    .skipLink:focus {
+      /* link styles when it's focused */
+    }
+    `,
+  },
+  {
+    Solution: CaseRoving,
     explanation: ``,
   },
 ];
